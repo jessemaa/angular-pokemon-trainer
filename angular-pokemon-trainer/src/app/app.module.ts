00b5d81bf5d2fca4f9cb07f9098fd0cd4e261198
jessemaa <mewwbae@gmail.com>
2021-03-09 15:20:52 +0200

    feat: added login-page component for homepage

--- angular-pokemon-trainer/src/app/app.module.ts
+++ angular-pokemon-trainer/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginPageComponent } from 'src/components/login-page/login.page.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
