2b0ee89e06f6969167cbd2b978eb1324ace3f5e9
jessemaa <mewwbae@gmail.com>
2021-03-11 18:02:37 +0200

    refactor: changed folder structure for components

--- angular-pokemon-trainer/src/app/app.module.ts
+++ angular-pokemon-trainer/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
-import { PokemonCatalogueComponent } from './pokemon-catalogue/pokemon-catalogue.component';
+import { PokemonCatalogueComponent } from '../components/pokemon-catalogue/pokemon-catalogue.component';
 
 @NgModule({
   declarations: [
--- angular-pokemon-trainer/src/components/pokemon-catalogue/pokemon-catalogue.component.ts
+++ angular-pokemon-trainer/src/components/pokemon-catalogue/pokemon-catalogue.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../services/data.service';
+import { DataService } from '../../app/services/data.service';
 import { Pokemon } from 'src/models/Pokemon'
 
 
