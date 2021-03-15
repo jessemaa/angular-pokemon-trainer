37d64c42eb99e683d35d751b05b180ee8625c27a
jessemaa <mewwbae@gmail.com>
2021-03-15 10:47:19 +0200

    feat: pokemon details page showing necessary stats and image, no styles

--- angular-pokemon-trainer/src/components/pokemon-detail/pokemon-detail.component.ts
+++ angular-pokemon-trainer/src/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonDetailService } from 'src/app/services/pokemon-detail.service';
+import { Pokemon } from 'src/models/Pokemon';
 
 @Component({
   selector: 'pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
   styleUrls: ['./pokemon-detail.component.css']
 })
-export class PokemonDetailComponent {
+export class PokemonDetailComponent implements OnInit {
+
+  private readonly pokemonName: string = '';
+
+  constructor(private readonly route: ActivatedRoute, private readonly pokemonDetailService: PokemonDetailService) {
+    this.pokemonName = this.route.snapshot.paramMap.get('name');
+  }
+
+  ngOnInit(): void {
+    this.pokemonDetailService.fetchPokemonByName(this.pokemonName);
+  }
+
+  get pokemon(): Pokemon {
+    return this.pokemonDetailService.pokemon;
+  }
 
 }
